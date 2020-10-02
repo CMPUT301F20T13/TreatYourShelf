51287ed1323e31ed5abe4112a6bc5259f32ca813
AnthonyMarkD <44737462+AnthonyMarkD@users.noreply.github.com>
2020-10-02 16:33:53 -0600

    Added in Room DataBase Functionality with BookListFragment

--- TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/BookList/BookListFragment.java
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/BookList/BookListFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +21,7 @@ import java.util.List;
 
 public class BookListFragment extends Fragment {
     private BookListAdapter bookListAdapter;
+    private BookListViewModel bookListViewModel;
 
     @Nullable
     @Override
@@ -27,29 +30,39 @@ public class BookListFragment extends Fragment {
 
         RecyclerView bookRv = view.findViewById(R.id.book_list_rv);
         bookRv.setLayoutManager(new LinearLayoutManager(getContext()));
-        bookListAdapter = createAdapter();
+        bookListAdapter = new BookListAdapter(new ArrayList<Book>());
         bookRv.setAdapter(bookListAdapter);
+        bookListViewModel = new ViewModelProvider(this).get(BookListViewModel.class);
 
+        bookListViewModel.bookList.observe(getViewLifecycleOwner(), new Observer<List<Book>>() {
+            @Override
+            public void onChanged(List<Book> books) {
+                if (books.isEmpty()) {
+                    // Initial list will be empty due to no data stored in the app, therefore we insert data into the database
+                    setInitialData();
+                }
+
+                bookListAdapter.setBookList(books);
+            }
+        });
 
         return view;
     }
 
-    private BookListAdapter createAdapter() {
-        ArrayList<Book> books = new ArrayList<>();
+    private void setInitialData() {
 
         Book book = new Book("Eat a Peach", "Gabe Ulla and David Chang");
-        books.add(book);
+        bookListViewModel.insert(book);
         book = new Book("A Killing Frost", "Seanan McGuire");
-        books.add(book);
+        bookListViewModel.insert(book);
         book = new Book("Our Malady", "Timothy Snyder");
-        books.add(book);
+        bookListViewModel.insert(book);
         book = new Book("Donald Trump v. The United States", "Michael S. Schmidt");
-        books.add(book);
+        bookListViewModel.insert(book);
         book = new Book("Star Wars: Thrawn Ascendancy (Book I: Chaos Rising)", "Timothy Zahn");
-        books.add(book);
+        bookListViewModel.insert(book);
         book = new Book("Fifty Words for Rain", "Asha Lemmie");
-        books.add(book);
+        bookListViewModel.insert(book);
 
-        return new BookListAdapter(books);
     }
 }
