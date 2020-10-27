800081eeceb27e6ede13497b1a3f1a31ee3cb426
kdwedage <kevin.d.wedage@gmail.com>
2020-10-27 17:37:41 -0600

    Added the login and sign up screens

---
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/login_screen/login_fragment.java
@@ -0,0 +1,38 @@
+package com.cmput301f20t13.treatyourshelf.ui.login_screen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.cmput301f20t13.treatyourshelf.R;
+import com.cmput301f20t13.treatyourshelf.data.Book;
+import com.cmput301f20t13.treatyourshelf.ui.search_page.SearchListAdapter;
+import com.cmput301f20t13.treatyourshelf.ui.search_page.SearchListViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class login_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        return view;
+    }
+
+
+}
