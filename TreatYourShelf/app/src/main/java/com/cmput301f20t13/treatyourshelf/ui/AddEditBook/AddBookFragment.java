2afbf9d5bcf050f608191aa84fce40d2eb866bc1
Charlie Chen <yc6@ualberta.ca>
2020-11-01 20:35:45 -0700

    AlertDialog for delete added, no functionality yet

--- TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/AddEditBook/AddBookFragment.java
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/AddEditBook/AddBookFragment.java
@@ -1,5 +1,6 @@
 package com.cmput301f20t13.treatyourshelf.ui.AddEditBook;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.cmput301f20t13.treatyourshelf.R;
@@ -27,6 +29,12 @@ public class AddBookFragment extends Fragment {
         EditText txtDesc = (EditText) view.findViewById(R.id.desc);
         EditText txtIsbn = (EditText) view.findViewById(R.id.isbn);
         Button addButton = (Button) view.findViewById(R.id.addbutton);
+        Button editButton = (Button) view.findViewById(R.id.editbutton);
+        Button deleteButton = (Button) view.findViewById(R.id.deletebutton);
+
+        addButton.setVisibility(View.VISIBLE);
+        editButton.setVisibility(View.INVISIBLE);
+        deleteButton.setVisibility(View.INVISIBLE);
         final int selected;
         /*
         for editing an existing book
@@ -38,7 +46,11 @@ public class AddBookFragment extends Fragment {
             txtAuthor.setText(book.getAuthor());
             txtDesc.setText(book.getDescription());
             txtIsbn.setText(book.getIsbn());
+            addButton.setVisibility(View.INVISIBLE);
+            editButton.setVisibility(View.VISIBLE);
+            deleteButton.setVisibility(View.VISIBLE);
         }
+
 
         /*
         adding a book
@@ -53,9 +65,32 @@ public class AddBookFragment extends Fragment {
             }
         });
 
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Delete entry")
+                        .setMessage("Are you sure you want to delete this entry?")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
+
 
         return view;
     }
+
 
     /*
     for editing an existing book
