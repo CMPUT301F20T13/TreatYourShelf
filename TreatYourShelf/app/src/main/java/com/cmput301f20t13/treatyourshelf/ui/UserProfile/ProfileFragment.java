dd8db45897421dab01bfc2ecbace5990992f243e
Luke Kapeluck <kapeluck@ualberta.ca>
2020-11-19 21:58:24 -0700

    Just a minor commit for button setup. Will do in a few minutes.

--- TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/UserProfile/ProfileFragment.java
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/ui/UserProfile/ProfileFragment.java
@@ -28,6 +28,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Get the necessary Views in the Fragment
         ImageView image = view.findViewById(R.id.profile_image);
         TextView username = view.findViewById(R.id.profile_username);
 
@@ -35,16 +36,18 @@ public class ProfileFragment extends Fragment {
         TextView email = view.findViewById(R.id.profile_email);
         TextView phone = view.findViewById(R.id.profile_phone);
 
+        // Get the current FirebaseAuth's email
         String profileEmail = "user1@gmail.com";
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             profileEmail = user.getEmail();
         }
 
+        // Get the details from the Firestore to set in the viewmodel
         ProfileViewModel profileViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(ProfileViewModel.class);
-
         profileViewModel.getProfileByEmailLiveData(profileEmail).observe(getViewLifecycleOwner(), Observable -> {});
 
+        // Set the View's values from the ViewModel's data
         profileViewModel.getProfile().observe(getViewLifecycleOwner(), profile -> {
             if (profile != null ) {
                 username.setText(profile.getUsername());
@@ -58,9 +61,14 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        // Add a listener for the edit button
         Button editButton = view.findViewById(R.id.profile_edit_button);
-        editButton.setOnClickListener(v -> {
-            // Not implemented yet
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Going to allow for editing of the username and email and picture
+                // TODO : Add functionality for editing the profile picture
+            }
         });
         return view;
     }
