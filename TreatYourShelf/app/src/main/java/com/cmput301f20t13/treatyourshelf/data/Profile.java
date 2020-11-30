bc5886613223516ab0a5b13ef9f59af1c1776a2f
Charlie Chen <yc6@ualberta.ca>
2020-11-29 22:45:42 -0700

    some comments

--- TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/data/Profile.java
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/data/Profile.java
@@ -2,6 +2,9 @@ package com.cmput301f20t13.treatyourshelf.data;
 
 import android.widget.ImageView;
 
+/**
+ * The Profile object that holds the information about a user's profile
+ */
 public class Profile {
     private String username;
     private String password;
@@ -9,6 +12,13 @@ public class Profile {
     private String phoneNumber;
     private ImageView profileImage;
 
+    /**
+     * the profile of the book
+     * @param username the username of the profile
+     * @param password the password of the profile
+     * @param email the email of the profile
+     * @param phoneNumber the phone number of the profile
+     */
     public Profile(String username, String password, String email, String phoneNumber) {
         this.username = username;
         this.password = password;
@@ -16,6 +26,9 @@ public class Profile {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * the default constructor for the Profile
+     */
     public Profile() {
         this.username = "default user";
         this.password = "password123";
@@ -23,42 +36,82 @@ public class Profile {
         this.phoneNumber = "555-555-5555";
     }
 
+    /**
+     * returns the username of the profile
+     * @return the username of the profile
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * sets the username of the profile
+     * @param username the provided username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * returns the password of the profile
+     * @return the password of the profile
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * sets the password of the profile
+     * @param password the provided password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * returns the email of the profile
+     * @return the email of the profile
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * sets the email of the profile
+     * @param email the provided email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * returns the phone number of the profile
+     * @return the phone number of the profile
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * sets the phone number of the profile
+     * @param phoneNumber the provided phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * returns the profile image of the profile
+     * @return the profile image of the profile
+     */
     public ImageView getProfileImage() {
         return profileImage;
     }
 
+    /**
+     * sets the profile image of the profile
+     * @param profileImage the provided profile image
+     */
     public void setProfileImage(ImageView profileImage) {
         this.profileImage = profileImage;
     }
--- TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/data/Request.java
+++ TreatYourShelf/app/src/main/java/com/cmput301f20t13/treatyourshelf/data/Request.java
@@ -1,5 +1,8 @@
 package com.cmput301f20t13.treatyourshelf.data;
 
+/**
+ * the request object holds the information about a request for a book
+ */
 public class Request {
 
     private String requester;
@@ -10,37 +13,93 @@ public class Request {
     private String author;
     private String title;
 
+    /**
+     * returns the requester for the request
+     * @return the requester for the request
+     */
     public String getRequester() {
         return requester;
     }
 
+    /**
+     * sets the requester for the request
+     * @param requester the provided requester
+     */
     public void setRequester(String requester) { this.requester = requester; }
 
+    /**
+     * returns the owner of the book for the request
+     * @return the owner of the book
+     */
     public String getOwner() { return owner; }
 
+    /**
+     * sets the owner of the book for the request
+     * @param owner the provided owner
+     */
     public void setOwner(String owner) { this.owner = owner; }
 
+    /**
+     * returns the BookId of the book for the request
+     * @return the bookid of the request
+     */
     public String getBookId() { return bookId; }
 
+    /**
+     * sets the BookId of the book for the request
+     * @param bookId the provided bookid
+     */
     public void setBookId(String bookId) { this.bookId = bookId; }
 
+    /**
+     * returns the status of the request
+     * @return the status of the book
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * sets the status of the request
+     * @param status the provided status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * returns the isbn of the book for the request
+     * @return the isbn of the book
+     */
     public String getIsbn() { return isbn; }
 
+    /**
+     * sets the isbn of the book for the request
+     * @param isbn the provided isbn
+     */
     public void setIsbn(String isbn) { this.isbn = isbn; }
 
+    /**
+     * returns the author of the book for the request
+     * @return the author of the book
+     */
     public String getAuthor() { return author; }
 
+    /**
+     * sets the author of the book for the request
+     * @param author the provided author
+     */
     public void setAuthor(String author) { this.author = author; }
 
+    /**
+     * returns the title of the book for the request
+     * @return the title of the book
+     */
     public String getTitle() { return title; }
 
+    /**
+     * sets the title of the book for the request
+     * @param title the provided title
+     */
     public void setTitle(String title) { this.title = title; }
 }
